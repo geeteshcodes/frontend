e2843084920e8e686a72f1863aa671e9501ff0f3
Geetesh Singh <geeteshsinghbhadauria@gmail.com>
2026-04-30 01:01:56 +0530

    Delete backend directory

--- backend/src/config/env.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-    PORT: process.env.PORT || 5000,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-    JWT_SECRET: process.env.JWT_SECRET
-}
